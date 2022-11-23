9793efb6cb36e8c16fb8bfe81f30b71d847af115
ryeongee <ryeongee24@gmail.com>
2022-11-23 17:02:53 +0900

    Update Code 'payment'
    Update
    - Refactoring Service "SpringKakaoPayService"

--- src/main/java/com/golfzonaca/officesharingplatform/service/payment/requestform/RequestBodyApproveConverter.java
+++ src/main/java/com/golfzonaca/officesharingplatform/service/payment/requestform/RequestBodyApproveConverter.java
@@ -2,6 +2,8 @@ package com.golfzonaca.officesharingplatform.service.payment.requestform;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.golfzonaca.officesharingplatform.domain.payment.CompanyId;
+import com.golfzonaca.officesharingplatform.domain.payment.kakaopay.KakaoPayReady;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,4 +22,15 @@ public class RequestBodyApproveConverter {
     private String partnerUserId; // 가맹점 회원 ID
     private String pgToken; // 결제 승인 요청을 인증하는 토큰
     private String totalAmount; // 상품 총액
+
+    public RequestBodyApproveConverter toEntity(String cid, String tid, String partnerOrderId, String partnerUserId, String pgToken, String totalAmount) {
+        return RequestBodyApproveConverter.builder()
+                .cid(cid)
+                .tid(tid)
+                .partnerOrderId(partnerOrderId)
+                .partnerUserId(partnerUserId)
+                .pgToken(pgToken)
+                .totalAmount(totalAmount)
+                .build();
+    }
 }
