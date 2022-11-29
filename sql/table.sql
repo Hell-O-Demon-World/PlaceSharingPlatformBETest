b2673f78992490d9064b7a12b7c08974f97d8d50
darkblose <darkblose@gmail.com>
2022-11-29 10:56:49 +0900

    update : sql

--- sql/table.sql
+++ sql/table.sql
@@ -5,53 +5,58 @@ create table address
     POSTALCODE varchar(5)   not null
 );
 
-create table mileage
-(
-    ID    bigint unsigned auto_increment primary key,
-    POINT bigint unsigned not null
-);
-
 create table company
 (
     ID              bigint unsigned auto_increment primary key,
+    ADDRESS_ID      bigint unsigned not null,
     COMPANY_LOGINID varchar(15)  not null,
     COMPANY_PW      varchar(100) not null,
     COMPANY_NAME    varchar(30)  not null,
     COMPANY_TEL     varchar(22)  not null,
     COMPANY_REGNUM  varchar(12)  not null,
     COMPANY_REPNAME varchar(20)  not null,
-    ADDRESS_ID      bigint unsigned not null,
-    constraint COMPANY_LOGINID unique (COMPANY_LOGINID),
-    constraint COMPANY_NAME unique (COMPANY_NAME),
-    constraint COMPANY_REGNUM unique (COMPANY_REGNUM),
-    constraint COMPANY_TEL unique (COMPANY_TEL),
+    constraint COMPANY_LOGINID
+        unique (COMPANY_LOGINID),
+    constraint COMPANY_NAME
+        unique (COMPANY_NAME),
+    constraint COMPANY_REGNUM
+        unique (COMPANY_REGNUM),
+    constraint COMPANY_TEL
+        unique (COMPANY_TEL),
     constraint FK_ADDRESS_TO_COMPANY_1
-        foreign key (ADDRESS_ID)
-            references address (ID)
-            on delete cascade
+        foreign key (ADDRESS_ID) references address (ID)
+            on update cascade on delete cascade
+);
+
+create table ratepoint
+(
+    ID          bigint unsigned auto_increment primary key,
+    RATINGPOINT float unsigned not null
 );
 
 create table place
 (
     ID                bigint unsigned auto_increment primary key,
     COMPANY_ID        bigint unsigned not null,
+    ADDRESS_ID        bigint unsigned not null,
+    RATEPOINT_ID      bigint unsigned not null,
     PLACE_NAME        varchar(30) not null,
     PLACE_DESCRIPTION varchar(50) not null,
     PLACE_OPENDAYS    varchar(50) not null,
     PLACE_START       time        not null,
     PLACE_END         time        not null,
     PLACE_ADDINFO     varchar(50) not null,
-    ADDRESS_ID        bigint unsigned not null,
     constraint PLACE_NAME
         unique (PLACE_NAME),
     constraint FK_ADDRESS_TO_PLACE_1
-        foreign key (ADDRESS_ID)
-            references address (ID)
-            on delete cascade,
+        foreign key (ADDRESS_ID) references address (ID)
+            on update cascade on delete cascade,
     constraint FK_COMPANY_TO_PLACE_1
-        foreign key (COMPANY_ID)
-            references company (ID)
-            on delete cascade
+        foreign key (COMPANY_ID) references company (ID)
+            on update cascade on delete cascade,
+    constraint FK_RATEPOINT_TO_PLACE_1
+        foreign key (RATEPOINT_ID) references ratepoint (ID)
+            on update cascade on delete cascade
 );
 
 create table room_kind
@@ -63,10 +68,50 @@ create table room_kind
         unique (ROOM_TYPE)
 );
 
+create table room
+(
+    ID           bigint unsigned auto_increment primary key,
+    ROOM_KIND_ID bigint unsigned not null,
+    PLACE_ID     bigint unsigned not null,
+    constraint FK_PLACE_TO_ROOM_1
+        foreign key (PLACE_ID) references place (ID)
+            on update cascade on delete cascade,
+    constraint FK_ROOM_KIND_TO_ROOM_1
+        foreign key (ROOM_KIND_ID) references room_kind (ID)
+            on update cascade on delete cascade
+);
+
+create table roomstatus
+(
+    ID      bigint unsigned auto_increment primary key,
+    ROOM_ID bigint unsigned not null,
+    STATUS  tinyint(1) default 1 not null,
+    constraint ROOM_ID
+        unique (ROOM_ID),
+    constraint FK_ROOM_TO_ROOMSTATUS_1
+        foreign key (ROOM_ID) references room (ID)
+            on update cascade on delete cascade
+);
+
+create table role
+(
+    ID   bigint unsigned not null primary key,
+    role enum ('ROLE_USER', 'ROLE_MANAGER') not null,
+    constraint ROLE
+        unique (role)
+);
+
+create table mileage
+(
+    ID    bigint unsigned auto_increment primary key,
+    POINT bigint unsigned not null
+);
+
 create table user
 (
     ID          bigint unsigned auto_increment primary key,
     MILEAGE_ID  bigint unsigned not null,
+    ROLE_ID     bigint unsigned default 1 not null,
     USER_MAIL   varchar(32)  not null,
     USER_PW     varchar(100) not null,
     USER_NAME   varchar(20)  not null,
@@ -78,90 +123,141 @@ create table user
     constraint USER_TEL
         unique (USER_TEL),
     constraint FK_MILEAGE_TO_USER_1
-        foreign key (MILEAGE_ID)
-            references mileage (ID)
-            on delete cascade
+        foreign key (MILEAGE_ID) references mileage (ID)
+            on update cascade on delete cascade,
+    constraint FK_ROLE_TO_USER_1
+        foreign key (ROLE_ID) references role (ID)
+            on update cascade on delete cascade
 );
 
-create table room
+create table refresh_token
 (
-    ID           bigint unsigned auto_increment primary key,
-    ROOM_KIND_ID bigint unsigned not null,
-    PLACE_ID     bigint unsigned not null,
-    COMPANY_ID   bigint unsigned not null,
-    TOTAL_NUM    int(3) unsigned not null,
-    ROOM_STATE   tinyint(1) not null,
-    constraint FK_COMPANY_TO_ROOM_1
-        foreign key (COMPANY_ID)
-            references company (ID)
-            on delete cascade,
-    constraint FK_PLACE_TO_ROOM_1
-        foreign key (PLACE_ID)
-            references place (ID)
-            on delete cascade,
-    constraint FK_ROOM_KIND_TO_ROOM_1
-        foreign key (ROOM_KIND_ID)
-            references room_kind (ID)
-            on delete cascade
+    ID            bigint unsigned auto_increment primary key,
+    USER_ID       bigint unsigned not null,
+    ENCODED_TOKEN varchar(147) not null,
+    constraint user_id
+        unique (USER_ID),
+    constraint FK_USER_TO_REFRESH_TOKEN_1
+        foreign key (USER_ID) references user (ID)
+            on update cascade on delete cascade
+);
+
+create table manager
+(
+    ID      bigint unsigned auto_increment primary key,
+    ROLE_ID bigint unsigned default 2 not null,
+    NAME    varchar(20)  not null,
+    EMAIL   varchar(32)  not null,
+    PW      varchar(100) not null,
+    TEL     varchar(22)  not null,
+    constraint EMAIL
+        unique (EMAIL),
+    constraint TEL
+        unique (TEL),
+    constraint FK_ROLE_TO_MANAGER_1
+        foreign key (ROLE_ID) references role (ID)
+            on update cascade on delete cascade
 );
 
 create table reservation
 (
     ID            bigint unsigned auto_increment primary key,
-    PLACE_ID      bigint unsigned not null,
     USER_ID       bigint unsigned not null,
     ROOM_ID       bigint unsigned not null,
-    ROOM_KIND_ID  bigint unsigned not null,
     RES_STARTDATE date not null,
     RES_STARTTIME time not null,
     RES_ENDDATE   date not null,
     RES_ENDTIME   time not null,
-    constraint FK_PLACE_TO_RESERVATION_1
-        foreign key (PLACE_ID)
-            references place (ID)
-            on delete cascade,
-    constraint FK_ROOM_KIND_TO_RESERVATION_1
-        foreign key (ROOM_KIND_ID)
-            references room_kind (ID)
-            on delete cascade,
     constraint FK_ROOM_TO_RESERVATION_1
-        foreign key (ROOM_ID)
-            references room (ID)
-            on delete cascade,
+        foreign key (ROOM_ID) references room (ID)
+            on update cascade on delete cascade,
     constraint FK_USER_TO_RESERVATION_1
-        foreign key (USER_ID)
-            references user (ID)
-            on delete cascade
+        foreign key (USER_ID) references user (ID)
+            on update cascade on delete cascade
 );
 
 create table payment
 (
-    ID           bigint unsigned auto_increment primary key,
-    USER_ID      bigint unsigned not null,
-    ROOM_ID      bigint unsigned not null,
-    PAY_DATE     date        not null comment 'API 결제 결과에서 가져오기',
-    PAY_TIME     time        not null comment 'API 결제 결과에서 가져오기',
-    PAY_PRICE    bigint unsigned not null comment '50000',
-    PAY_STATUS   enum ('PREPAYMENT', 'POSTPAYMENT') not null comment '선결제, 현장결제',
-    PAY_MILEAGE  bigint unsigned not null,
-    PAY_TYPE     enum ('DEPOSIT', 'BALANCE', 'FULLPAYMENT') not null,
-    PAY_API_CODE varchar(30) not null comment 'API 결제를 통해 생성되는 거래 번호',
-    constraint FK_USER_TO_PAYMENT_1
-        foreign key (USER_ID)
-            references user (ID)
-            on delete cascade,
-    constraint FK_ROOM_TO_PAYMENT_1
-        foreign key (ROOM_ID)
-            references ROOM (ID)
-            on delete cascade
+    ID             bigint unsigned auto_increment primary key,
+    RESERVATION_ID bigint unsigned not null,
+    PAY_DATE       date        not null comment 'API 결제 결과에서 가져오기',
+    PAY_TIME       time        not null comment 'API 결제 결과에서 가져오기',
+    PAY_PRICE      bigint unsigned not null comment '50000',
+    PAY_MILEAGE    bigint unsigned not null,
+    PAY_STATUS     enum ('PREPAYMENT', 'POSTPAYMENT') not null comment '선결제, 현장결제',
+    SAVED_MILEAGE  bigint unsigned not null,
+    PAY_TYPE       enum ('DEPOSIT', 'BALANCE', 'FULLPAYMENT') not null,
+    PAY_API_CODE   varchar(30) not null comment 'API 결제를 통해 생성되는 거래 번호',
+    constraint PAY_API_CODE
+        unique (PAY_API_CODE),
+    constraint FK_RESERVATION_TO_PAYMENT_1
+        foreign key (RESERVATION_ID) references reservation (ID)
+            on update cascade on delete cascade
 );
-create table refresh_token
+
+create table rating
 (
-    ID            bigint unsigned auto_increment primary key ,
+    ID             bigint unsigned auto_increment primary key,
+    RESERVATION_ID bigint unsigned not null,
+    RATING_SCORE   float unsigned not null,
+    RATING_REVIEW  varchar(200) not null,
+    RATING_TIME    datetime     not null,
+    constraint FK_RESERVATION_TO_RATING_1
+        foreign key (RESERVATION_ID) references reservation (ID)
+            on update cascade on delete cascade
+);
+
+create table inquiry
+(
+    ID        bigint unsigned auto_increment primary key,
+    USER_ID   bigint unsigned not null,
+    TITLE     varchar(40)  not null,
+    QUESTION  varchar(400) not null,
+    WRITETIME datetime     not null,
+    constraint FK_USER_TO_INQUIRY_1
+        foreign key (USER_ID) references user (ID)
+            on update cascade on delete cascade
+);
+
+create table inquirystatus
+(
+    ID         bigint unsigned auto_increment primary key,
+    INQUIRY_ID bigint unsigned not null,
+    STATUS     tinyint(1) default 0 not null,
+    constraint INQUIRY_ID
+        unique (INQUIRY_ID),
+    constraint FK_INQUIRY_TO_INQUIRYSTATUS_1
+        foreign key (INQUIRY_ID) references inquiry (ID)
+            on update cascade on delete cascade
+);
+
+create table answer
+(
+    ID         bigint unsigned auto_increment primary key,
+    INQUIRY_ID bigint unsigned not null,
+    MANAGER_ID bigint unsigned not null,
+    ANSWER     varchar(400) not null,
+    constraint INQUIRY_ID
+        unique (INQUIRY_ID),
+    constraint FK_ANSWER_TO_ANSWER_1
+        foreign key (MANAGER_ID) references manager (ID)
+            on update cascade on delete cascade,
+    constraint FK_INQUIRY_TO_ANSWER_1
+        foreign key (INQUIRY_ID) references inquiry (ID)
+            on update cascade on delete cascade
+);
+
+create table comment
+(
+    ID            bigint unsigned auto_increment primary key,
+    RATING_ID     bigint unsigned not null,
     USER_ID       bigint unsigned not null,
-    ENCODED_TOKEN VARCHAR(256) not null,
-    constraint FK_USER_TO_REFRESH_TOKEN_1
-        foreign key (USER_ID)
-            references user (ID)
-            on delete cascade
+    CONTEXT       varchar(40) not null,
+    WRITEDATETIME datetime    not null,
+    constraint FK_PLACE_TO_COMMENT_1
+        foreign key (RATING_ID) references rating (ID)
+            on update cascade on delete cascade,
+    constraint FK_USER_TO_COMMENT_1
+        foreign key (USER_ID) references user (ID)
+            on update cascade on delete cascade
 );
