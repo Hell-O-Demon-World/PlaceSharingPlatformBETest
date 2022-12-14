0fe5485d74cdb4ced942a1e6e39bd9c0d9bdfb75
darkblose <darkblose@gmail.com>
2022-12-14 10:54:32 +0900

    update : Place Controller

--- src/main/java/com/golfzonaca/officesharingplatform/web/place/dto/PlaceCoordinate.java
+++ src/main/java/com/golfzonaca/officesharingplatform/web/place/dto/PlaceCoordinate.java
@@ -1,8 +1,10 @@
 package com.golfzonaca.officesharingplatform.web.place.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class PlaceCoordinate {
     private Double lng;
     private Double lat;
