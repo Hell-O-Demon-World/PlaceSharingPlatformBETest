a8f1185f3ad087a94c1088e537095b1ea6ddfb7d
darkblose <darkblose@gmail.com>
2022-12-14 11:37:59 +0900

    update : native query

--- src/main/java/com/golfzonaca/officesharingplatform/repository/rating/SpringJpaRatingRepository.java
+++ src/main/java/com/golfzonaca/officesharingplatform/repository/rating/SpringJpaRatingRepository.java
@@ -11,6 +11,6 @@ public interface SpringJpaRatingRepository extends JpaRepository<Rating, Long> {
     @Query(value = "select rating.ID, rating.RATING_REVIEW, rating.RATING_SCORE, rating.RATING_TIME, rating.RESERVATION_ID from rating join reservation res on res.ID = rating.RESERVATION_ID join room on res.ROOM_ID = room.ID join place p on room.PLACE_ID = p.ID WHERE p.id = :placeId ORDER BY rating.RATING_TIME DESC", nativeQuery = true)
     List<Rating> countByPlace(@Param("placeId") Long placeId);
 
-    @Query(value = "select rating.ID, rating.RATING_REVIEW, rating.RATING_SCORE, rating.RATING_TIME, rating.RESERVATION_ID from rating join reservation res on res.ID = rating.RESERVATION_ID join room on res.ROOM_ID = room.ID join place p on room.PLACE_ID = p.ID WHERE p.id = :placeId ORDER BY rating.RATING_TIME DESC limit :page,4", nativeQuery = true)
+    @Query(value = "select rating.ID, rating.RATING_REVIEW, rating.RATING_SCORE, rating.RATING_TIME, rating.RESERVATION_ID from rating join reservation res on res.ID = rating.RESERVATION_ID join room on res.ROOM_ID = room.ID join place p on room.PLACE_ID = p.ID WHERE p.id = :placeId ORDER BY rating.RATING_TIME DESC limit :page-1,4", nativeQuery = true)
     List<Rating> findAllByPlaceWithPagination(@Param("placeId") Long placeId, @Param("page") Integer page);
 }
