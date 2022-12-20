76d805cc5a44b9748dbc601b9d122f7b8145fd9f
darkblose <darkblose@gmail.com>
2022-12-20 20:45:55 +0900

    update : Payment Failure Exception

--- src/main/java/com/golfzonaca/officesharingplatform/repository/refund/QueryRefundRepository.java
+++ src/main/java/com/golfzonaca/officesharingplatform/repository/refund/QueryRefundRepository.java
@@ -1,4 +1,30 @@
 package com.golfzonaca.officesharingplatform.repository.refund;
 
+import com.golfzonaca.officesharingplatform.domain.Payment;
+import com.golfzonaca.officesharingplatform.domain.Refund;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+import static com.golfzonaca.officesharingplatform.domain.QRefund.refund;
+
+@Transactional
+@Repository
 public class QueryRefundRepository {
+    private final JPAQueryFactory query;
+
+    public QueryRefundRepository(EntityManager em) {
+        this.query = new JPAQueryFactory(em);
+    }
+
+
+    public Optional<Refund> findByPayment(Payment payment) {
+        return Optional.ofNullable(query
+                .selectFrom(refund)
+                .where(refund.payment.eq(payment))
+                .fetchFirst());
+    }
 }
--- src/main/java/com/golfzonaca/officesharingplatform/repository/refund/RefundRepository.java
+++ src/main/java/com/golfzonaca/officesharingplatform/repository/refund/RefundRepository.java
@@ -1,8 +1,13 @@
 package com.golfzonaca.officesharingplatform.repository.refund;
 
+import com.golfzonaca.officesharingplatform.domain.Payment;
 import com.golfzonaca.officesharingplatform.domain.Refund;
+
+import java.util.Optional;
 
 public interface RefundRepository {
 
     void save(Refund refund);
+
+    Optional<Refund> findByPayment(Payment payment);
 }
