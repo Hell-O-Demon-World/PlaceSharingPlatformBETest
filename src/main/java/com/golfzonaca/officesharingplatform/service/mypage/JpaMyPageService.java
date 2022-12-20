95e2c1c5613a6f17d108dca5d42bb947a271a6bb
darkblose <darkblose@gmail.com>
2022-12-20 14:17:10 +0900

    update : CacheEvict

--- src/main/java/com/golfzonaca/officesharingplatform/service/mypage/JpaMyPageService.java
+++ src/main/java/com/golfzonaca/officesharingplatform/service/mypage/JpaMyPageService.java
@@ -38,6 +38,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -193,6 +194,7 @@ public class JpaMyPageService implements MyPageService {
         return putMileageData(findUser, page, items);
     }
 
+    @CacheEvict(cacheNames = {"resDataByPlaceAndTypeAndDate", "resAllDataByPlaceAndTypeAndDate"}, allEntries = true)
     @Override
     public void clearPreoccupiedReservation(Long userId) {
         User user = userRepository.findById(userId);
